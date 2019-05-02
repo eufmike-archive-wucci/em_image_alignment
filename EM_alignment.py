a196252a45fa8fc9a10c51e81d1abc0ad12b02f2
Michael Shih <sc2.shih@gmail.com>
2019-05-02 14:12:06 -0500

    m

---
+++ EM_alignment.py
@@ -0,0 +1,39 @@
+# %load_ext autoreload
+# %autoreload 2
+
+#%%
+import os, sys, re
+import pprint
+from shutil import copyfile
+from core.fileop import DirCheck, ListFiles, SplitAll, SortByFolder
+
+# functions ============================================
+
+
+# ========================================================
+
+#%%
+path = '/Volumes/LaCie_DataStorage/em_alignment_test'
+raw_dir = 'raw'
+sub_dir = 'KO'
+raw_path = os.path.join(path, raw_dir, sub_dir)
+print(raw_path)
+
+op_dir = 'output'
+oppath = os.path.join(path, op_dir, sub_dir)
+
+DirCheck(oppath)
+
+#%%
+filepath = ListFiles(raw_path, '.tif')['fileabslist']
+print(filepath)
+
+#%%
+cleaned_filelist = SortByFolder(filepath, 'Tile_')
+print([item.filename for item in cleaned_filelist])
+
+#%%
+# copy files to the new output folder
+for item in cleaned_filelist:
+    print(item.filename)
+    copyfile(item.filepath, os.path.join(oppath, 'image_' + str(item.index).zfill(4) + '.tif'))
