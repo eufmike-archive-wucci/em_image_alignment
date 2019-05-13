2e0a0cffe7fd887bdd30ab63fbc2658c487c869c
Michael Shih <sc2.shih@gmail.com>
2019-05-13 16:30:19 -0500

    stitching_rename_done

--- TEM_stichingrename.py
+++ TEM_stichingrename.py
@@ -5,27 +5,69 @@
 #%%
 import os, sys, re
 import numpy as np
+import pandas as pd
 import cv2
 from PIL import Image
 import pprint
 from shutil import copyfile
 from core.fileop import DirCheck, ListFiles, ListFolders, SplitAll, SortByFolder
+from core.imgstitching import IdxConverter
 
 #%%
 path = '/Volumes/LaCie_DataStorage/Asensio_Lab'
-raw_dir = 'alignment_opencv_01_std_8bit'
-sub_dir = 'KO_2_reordered_rename'
-ippath = os.path.join(path, raw_dir, sub_dir)
+wd_dir = 'TEM_stitching_test'
+ip_dir = 'output'
+ippath = os.path.join(path, wd_dir, ip_dir)
 print(ippath)
 
 dircheck = []
-op_dir = 'alignment_opencv_02'
-opmatch_dir = 'alignment_match_opencv_02'
-oppath = os.path.join(path, op_dir, sub_dir)
-oppath_match = os.path.join(path, opmatch_dir, sub_dir)
+op_dir = 'ouput_reordered'
+oppath = os.path.join(path, wd_dir, op_dir)
 dircheck.append(oppath)
-dircheck.append(oppath_match)
-
 DirCheck(dircheck)
 
-#
+#%%
+# load par
+par_dir = 'par'
+par_path = os.path.join(path, wd_dir, par_dir, 'tile_dimension.csv')
+DimData = pd.read_csv(par_path)
+display(DimData)
+DimData['filename'] = DimData['filename'].astype(str)
+DimData.dtypes
+#%%
+for i in ListFolders(ippath):
+    ippath_tmp = os.path.join(ippath, i)
+    oppath_tmp = os.path.join(oppath, i)
+    DirCheck(oppath_tmp)
+
+    dimdata = DimData[DimData['group'] == i]
+    display(dimdata)
+    
+    for j in ListFolders(ippath_tmp):
+        ippath_img_tmp = os.path.join(ippath_tmp, j)
+        oppath_img_tmp = os.path.join(oppath_tmp, j)
+        DirCheck(oppath_img_tmp)
+        print(j)
+        m = re.search('^img_(0*[1-9][0-9]*)$', j)
+        if m:
+            found = m.group(1)
+            found = found.strip('0')
+            print(found)
+        filename_list = list(dimdata['filename'])
+        idx = filename_list.index(found)
+        print(idx)
+        
+        tile_dim = (dimdata['x'][idx], dimdata['y'][idx])
+        box_dim = (3, 3)
+        
+        filelist, fileabslist = ListFiles(ippath_img_tmp, '.tif') 
+        print(filelist)
+
+        for idx in range(len(filelist)):
+            ip_filename = j + '_' + str(idx).zfill(4) + '.tif'
+            op_filename = j + '_' + str(IdxConverter(idx, tile_dim, box_dim)).zfill(4) + '.tif'
+            print(ip_filename)
+            ip_path = os.path.join(ippath, i, j, ip_filename)
+            print(op_filename)
+            op_path = os.path.join(oppath, i, j, op_filename)
+            copyfile(ip_path, op_path)
--- core/imgstitching.py
+++ core/imgstitching.py
@@ -1,30 +1,17 @@
 #%%
-%load_ext autoreload
-%autoreload 2
-
-#%%
 import os, sys, re
 import numpy as np
 import cv2
 from PIL import Image
 import pprint
 
-
-#%%
-order = list(range(25))
-print(order)
-f_x = 8
-f_y = 8
-box_x = 3
-box_y = 3
-
-def idxconverter(idx, tile_dim = (f_x, f_y), box_dim = (box_x, box_y)):
+def IdxConverter(idx, tile_dim, box_dim):
     f_x, f_y = tile_dim
     tile_size = f_x * f_y
     box_x, box_y = box_dim
     box_size = box_x * box_y
 
-    print(tile_size)
+    # print(tile_size)
     x_com_box = f_x//box_x
     x_rem = f_x%box_x
     if x_rem > 0:
@@ -43,7 +30,7 @@ def idxconverter(idx, tile_dim = (f_x, f_y), box_dim = (box_x, box_y)):
     vert_incom_box = x_incom_box * y_com_box
     hori_incom_box = y_incom_box * x_com_box
     xy_rem = x_incom_box * y_incom_box
-    
+    '''
     print('x_com_box: {}'.format(x_com_box))
     print('x_incom_box: {}'.format(x_incom_box))
     print('y_com_box: {}'.format(y_com_box))
@@ -55,19 +42,19 @@ def idxconverter(idx, tile_dim = (f_x, f_y), box_dim = (box_x, box_y)):
     print('vert_incom_box: {}'.format(vert_incom_box))
     print('hori_incom_box: {}'.format(hori_incom_box))
     print('xy_rem: {}'.format(xy_rem))
-
+    '''
     area_com = x_com_box * y_com_box * box_size
     area_incom_hori = hori_incom_box * y_rem * box_x
     area_incom_vert = vert_incom_box * x_rem * box_y
 
     if idx > (area_com + area_incom_hori + area_incom_vert - 1):
-        print('area_rem')
+        # print('area_rem')
         box_local = idx - (area_com + area_incom_hori + area_incom_vert)
         idx_absidx_x = box_local // y_rem + x_com_box * box_x
         idx_absidx_y = box_local % y_rem + y_com_box * box_y
 
     elif idx > (area_com + area_incom_hori - 1 ):
-        print('area_vert')
+        # print('area_vert')
         box_idx = idx // (x_rem * box_y)
         idx_in_area_vert =  idx - (area_com + area_incom_hori)
         box_local = idx_in_area_vert % (x_rem * box_y)
@@ -76,35 +63,29 @@ def idxconverter(idx, tile_dim = (f_x, f_y), box_dim = (box_x, box_y)):
         idx_absidx_y = box_local % box_y + (idx_in_area_vert // (x_rem * box_y)) * box_y
 
     else:
-        print('area_vert or com')
+        # print('area_vert or com')
         box_idx_x = idx // (box_size * y_com_box + y_rem * box_x)
         box_idx_y = idx % (box_size * y_com_box + y_rem * box_x) // box_size
         # print(box_idx_x)
         # print(box_idx_y) 
 
         if box_idx_y == y_com_box:
-            print('box_idx_y == y_com_box')
+            # print('box_idx_y == y_com_box')
             box_local = idx % (box_size * y_com_box + y_rem * box_x) % box_size
             # print(box_local)
             idx_absidx_x = box_local // y_rem + box_idx_x * box_x
             idx_absidx_y = box_local % y_rem + box_idx_y * box_y
         else:
-            print('box_idx_y != y_com_box')
+            # print('box_idx_y != y_com_box')
             box_local = idx % (box_size * y_com_box + y_rem * box_x) % box_size
             # print(box_local)
             idx_absidx_x = box_local // box_y + box_idx_x * box_x
             idx_absidx_y = box_local % box_y + box_idx_y * box_y
     
-    print('idx_absidx_x: {}'.format(idx_absidx_x))
-    print('idx_absidx_y: {}'.format(idx_absidx_y))
+    # print('idx_absidx_x: {}'.format(idx_absidx_x))
+    # print('idx_absidx_y: {}'.format(idx_absidx_y))
     
     idx_new = idx_absidx_x * f_y + idx_absidx_y
     return idx_new
+
     
-
-#%%
-idx = 37
-idx_new = idxconverter(idx, tile_dim = (f_x, f_y), box_dim = (box_x, box_y))
-print(idx_new)
-
-#%%
